a60954c9e59ebe294f7722ec2173de710fa86201
Paolo Fragomeni <paolo@async.ly>
2014-11-30 15:29:22 +0100

    playing with layouts

--- client.js
+++ client.js
@@ -7,15 +7,23 @@ var root = '../../';
 var menu = require(root + 'lib/menu')();
 var header = require(root + 'lib/header');
 var query = require(root + 'lib/query');
+var put = require(root + 'lib/put');
+var connections = require(root + 'lib/connections');
 
 var config = require(root + 'defaults.json');
 var rc = require('rc')('levelui', config);
-var db = require(root + 'lib/db')('../db', config);
+var db = require(root + 'lib/db');
+
+var database = {
+  handle: db('../db', config)
+};
 
 domready(function() {
 
-  header(db, config);
-  query(db, config);
+  header.init(database, config);
+  query.init(database, config);
+  put.init(database, config);
+  connections.init(database, config);
 
 });
 
--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 var app = require('app');
-var ipc = require('ipc');
-var db = require('./lib/db');
 
+var dialog = require('dialog')
 var BrowserWindow = require('browser-window');
 var cr = require('crash-reporter').start();
 
 var mainWindow = null;
-
-ipc.on('asynchronous-message', function(event, arg) {
-  console.log(arg);  // prints "ping"
-  event.sender.send('asynchronous-reply', 'pong');
-});
 
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin') {
@@ -23,10 +17,15 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({ width: 900, height: 600, 'min-width': 900, 'min-height': 600, frame: false });
 
   // dialog.showMessageBox([browserWindow], [options], [callback]);
-
-
-
   mainWindow.loadUrl('file://' + __dirname + '/assets/html/index.html');
+
+  //
+  // Explodes... atom-shell is a little broken.
+  //
+  //dialog.showOpenDialog(d, { title: 'ok', defaultPath: '/', properties: ['openDirectory'] }, function(savePath) { 
+  //  console.log(savePath); 
+  //});
+
 
   mainWindow.on('closed', function() {
     mainWindow = null;
